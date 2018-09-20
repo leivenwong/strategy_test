b53b62e9a756b8a03666d2f4f088770e16e55bcd
leivenwong <13852904@qq.com>
2018-09-20 20:05:41 +0800

    restructure

--- functions.py
+++ functions.py
@@ -6,8 +6,11 @@ from sqlalchemy import create_engine
 from settings import Settings
 
 def read_sql(ai_settings):
+    """read data from sql database"""
+    print("Enter Mysql")
     engine = create_engine(ai_settings.sql_path)
     df = pd.read_sql(sql="select * from "+ai_settings.fetch_table, con=engine)
+    print("Out Mysql")
     return df
 
 def read_file(ai_settings):
@@ -18,6 +21,7 @@ def read_file(ai_settings):
     return loads
 
 def compute_ma(data,cycle):
+    """compute ma value list"""
     ma_cycle = list(range(len(data)))
     for i in range(cycle):
         ma_cycle[i] = data[0]
@@ -25,17 +29,26 @@ def compute_ma(data,cycle):
         ma_cycle[i] = (sum(data[i - cycle:i])) / cycle
     return ma_cycle
 
-def compute_net_value(data, direction):
+def compute_net_value(data, direction, ai_setting):
+    """compute net value list"""
     net_value = list(range(len(data)))
     net_value[0] = 1
+    fee_mark = 0
     for i in range(1, len(data)):
+        if direction[i - 1] != direction[i]:
+            fee_mark = ai_setting.trade_fee
+        else:
+            fee_mark = 0
         if direction[i - 1] == 1:
-            net_value[i] = net_value[i - 1] * ((data[i] - data[i - 1]) / data[i - 1] + 1)
+            net_value[i] = net_value[i - 1] * ((data[i] - data[i - 1])
+                / data[i - 1] + 1 - fee_mark)
         elif direction[i - 1] == -1:
-            net_value[i] = net_value[i - 1] * ((data[i - 1] - data[i]) / data[i - 1] + 1)
+            net_value[i] = net_value[i - 1] * ((data[i - 1] - data[i])
+                / data[i - 1] + 1 - fee_mark)
         else:
             net_value[i] = net_value[i - 1]
-        print(net_value[i])
+        print("Net value: " + str(net_value[i]))
+        print("Fee: " + str(fee_mark))
     return net_value
 
 def frofit_per(data_close):
@@ -47,6 +60,7 @@ def frofit_per(data_close):
     return profit_per
 
 def set_xlable_visible(data):
+    """set x lable's"visible config"""
     ax = plt.gca()
     visible_count = int(len(data) / 5)
     for ind, label in enumerate(ax.xaxis.get_ticklabels()):
@@ -58,7 +72,26 @@ def set_xlable_visible(data):
     for label in ax.xaxis.get_ticklabels():
         label.set_rotation(0)
 
-def date_left(data):
+def date_format(data):
+    """set date format which wanted"""
     for i in range(len(data)):
        data[i] = data[i][0:10]
     return  data
+
+def draw_plot(ai_settings, net_value, target_net_value, data_date):
+    # set window size for plot
+    plt.figure(dpi=128, figsize=(12, 6))
+    # set data for plot
+    plt.subplot(211)
+    plt.title("Stategy net value", fontsize=12)
+    plt.plot(net_value, color='Red')
+
+    plt.subplot(212)
+    plt.title(ai_settings.fetch_table + " index net value", fontsize=12)
+    plt.plot(range(len(data_date)), target_net_value)
+    plt.xticks(range(len(data_date)), data_date, rotation=0)
+
+    # set numbers visible for x lable
+    set_xlable_visible(target_net_value)
+
+    plt.show()
--- index_test.py
+++ index_test.py
@@ -11,57 +11,45 @@ import functions as fuc
 #initiate settings
 ai_settings = Settings()
 
-#read raw hs300 data
+#read raw data
 target = fuc.read_sql(ai_settings)
 target = pd.DataFrame(target)
 
 #select close price for compute
 data_close = target.loc[0:, ai_settings.fetch_close]
 data_date = target.loc[0:, ai_settings.fetch_date]
-data_date = fuc.date_left(data_date)
+data_date = fuc.date_format(data_date)
 
+#compute net value of target index
 target_profit_day = fuc.frofit_per(data_close)
 target_direction = [1] * len(data_close)
-target_net_value = fuc.compute_net_value(data_close, target_direction)
+target_net_value = fuc.compute_net_value(data_close, target_direction, ai_settings)
 
-#compute 5 days ma
-ma_5 = fuc.compute_ma(data_close, 5)
+#compute ma
+ma_20 = fuc.compute_ma(data_close, 20)
 ma_10 = fuc.compute_ma(data_close, 10)
 
 #initiate variate
 direction = [0] * len(data_close)
 trade_times = 0
 
-#compute marks
+#compute direction
 for i in range(len(data_close)):
-    if ma_5[i] >= ma_10[i]:
+    if ma_10[i] >= ma_20[i]:
         direction[i] = 1
     else:
         direction[i] = -1
     if direction[i] != direction[i - 1]:
         trade_times += 1
 
-#compute profit
-net_value = fuc.compute_net_value(data_close, direction)
+#compute net value of strategy
+net_value = fuc.compute_net_value(data_close, direction, ai_settings)
 
+#print result
 print("Strategy net value: "+str(net_value[-1]))
 print("Trade times: "+str(trade_times))
 print(ai_settings.fetch_table+" net value: "+str(target_net_value[-1]))
 
-#set window size for plot
-plt.figure(dpi=128, figsize=(12, 6))
-
-#set data for plot
-plt.subplot(211)
-plt.title("Stategy net value",fontsize=12)
-plt.plot(net_value, color='Red')
-
-plt.subplot(212)
-plt.title(ai_settings.fetch_table+" index net value",fontsize=12)
-plt.plot(range(len(data_date)), target_net_value)
-plt.xticks(range(len(data_date)), data_date, rotation=0)
-
-#set numbers visible for x lable
-fuc.set_xlable_visible(target_net_value)
-
-plt.show()
+#draw plot according to settings
+if ai_settings.draw_plot:
+    fuc.draw_plot(ai_settings, net_value, target_net_value,data_date)
